32696f937585d6beb8fc9616936ce9d98cb51773
Justin Shaw <realjustinshaw@gmail.com>
2021-08-30 14:20:27 -0700

    fix: update tailwind purging strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
